cfbc8fd2e37e5a4b4708eab867acf09acf0f9016
yjydist <yjydist@gmail.com>
2025-01-21 14:12:42 +0800

    init

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -U fish_greeting
+
+alias mc="macchina -o host -o kernel -o terminal -o shell -o packages -o memory"
+alias ls="eza -l"
