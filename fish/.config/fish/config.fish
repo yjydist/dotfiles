ee5e34a6fd24bb40ae447638874adc146c86a451
yjydist <yjydist@gmail.com>
2025-01-26 15:35:39 +0800

    modify

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,5 +4,11 @@ end
 
 set -U fish_greeting
 
+# set PATH
+set -gx PATH /opt/homebrew/bin $PATH
+set -gx PATH $PATH /Users/yjydist/.spicetify
+set -gx PATH /opt/homebrew/opt/openjdk/bin $PATH
+set -gx PATH /usr/local/bin $PATH
+# set alias
 alias mc="macchina -o host -o kernel -o terminal -o shell -o packages -o memory"
 alias ls="eza -l"
